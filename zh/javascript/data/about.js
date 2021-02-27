d97de6c58dfcdf892e6bee1ed887695afe32c229
ehcoo <garay.le8@gmail.com>
2021-02-27 13:04:40 +0800

    updat the team

--- zh/javascript/data/about.js
+++ zh/javascript/data/about.js
@@ -2,8 +2,8 @@ var aboutData = {
     sections:[
         {
             contents:[
-                " Our on-campus research capabilities are enhanced through the work of MIT\n" +
-                " <a href=\"https://www.ll.mit.edu/\" target=\"_blank\">Lincoln Laboratory</a>." 
+                "关于我们" 
+               
 				,
                 " With ingenuity and drive, our graduates have invented fundamental technologies, launched\n" +
                 " new industries, and created millions of American jobs. At the same time, and without the\n"
--- zh/javascript/data/team.js
+++ zh/javascript/data/team.js
@@ -7,52 +7,42 @@ var teamData = {
                {
                     alt: "aaaaa",
                     src: "../files/images/202003/MIT-Astronaut-Isolation-SL.jpg",
-                    name: "李汝佳",
+                    name: "",
                     desc:  "entrepreneurship on Earth"
                },
                  {
                     alt: "aaaaa",
                     src: "../files/images/202003/MIT-Astronaut-Isolation-SL.jpg",
-                    name: "李汝佳",
+                    name: "",
                     desc:  " entrepreneurship on Earth"
                },
                  {
                     alt: "aaaaa",
                     src: "../files/images/202003/MIT-Astronaut-Isolation-SL.jpg",
-                    name: "李汝佳",
+                    name: "",
                     desc:  " entrepreneurship on Earth"
                },
                  {
                     alt: "aaaaa",
                     src: "../files/images/202003/MIT-Astronaut-Isolation-SL.jpg",
-                    name: "李汝佳",
+                    name: "",
                     desc:  " entrepreneurship on Earth"
                },
                  {
                     alt: "aaaaa",
                     src: "../files/images/202003/MIT-Astronaut-Isolation-SL.jpg",
-                    name: "李汝佳",
+                    name: "",
                     desc:  " entrepreneurship on Earth"
                }
                
         ]
-    }
-	/*
+    },
+	
     sections: [
         {
-            name: "王菲菲",
+            name: "区块链研究团队",
             descs: [
-                "MIT is at the&nbsp;center of one of the most vibrant hubs of innovation and\n" +
-                "                                entrepreneurship on Earth. To make our community the world’s most stimulating and\n" +
-                "                                supportive academic environment for innovation, we offer classes, mentorship, guidance,\n" +
-                "                                and <a href=\"https://innovation.mit.edu/resources/\" target=\"_blank\">resources</a>\n" +
-                "                                tailored for student, faculty, postdoc, and alumni entrepreneurs. We also cultivate the\n" +
-                "                                strength of our regional innovation ecosystem, in part through an ambitious, decade-long\n" +
-                "                                redevelopment initiative in <a href=\"http://kendallsquare.org/\" target=\"_blank\">Kendall\n" +
-                "                                    Square</a>.",
-                "supportive academic environment for innovation, we offer classes, mentorship, guidance,\\n\" +\n" +
-                "                                and <a href=\\\"https://innovation.mit.edu/resources/\\\" target=\\\"_blank\\\">resources</a>\\n\" +\n" +
-                "                                tailored for student,"
+                "MIT is at the&nbsp;center of one of the most vibrant hubs of innovation and\n"
             ],
             resource: {
                 type: "text",
@@ -66,69 +56,26 @@ var teamData = {
                         linkUrl: "https://alum.mit.edu/"
                     },
                     {
-                        title: "热点排行榜2Alumni Career Resources",
+                        title: "热点排行榜",
                         linkUrl: "https://alum.mit.edu/"
                     },
                     {
-                        title: "热点排行榜2Lifelong Learning",
+                        title: "热点排行榜",
                         linkUrl: "https://alum.mit.edu/"
                     },
                     {
-                        title: "热点排行榜2Give to MIT",
+                        title: "热点排行榜",
                         linkUrl: "http://entrepreneurship.mit.edu/"
                     },
                     {
-                        title: "热点排行榜2MIT Alumni Association",
+                        title: "热点排行榜",
                         linkUrl: "https://alum.mit.edu/"
                     }
                 ]
             }
-        },
-        {
-            name: "章子怡",
-            descs: [
-                "MIT is at the&nbsp;center of one of the most vibrant hubs of innovation and\n" +
-                "                                entrepreneurship on Earth. To make our community the world’s most stimulating and\n" +
-                "                                supportive academic environment for innovation, we offer classes, mentorship, guidance,\n" +
-                "                                and <a href=\"https://innovation.mit.edu/resources/\" target=\"_blank\">resources</a>\n" +
-                "                                tailored for student, faculty, postdoc, and alumni entrepreneurs. We also cultivate the\n" +
-                "                                strength of our regional innovation ecosystem, in part through an ambitious, decade-long\n" +
-                "                                redevelopment initiative in <a href=\"http://kendallsquare.org/\" target=\"_blank\">Kendall\n" +
-                "                                    Square</a>.",
-                "supportive academic environment for innovation, we offer classes, mentorship, guidance,\\n\" +\n" +
-                "                                and <a href=\\\"https://innovation.mit.edu/resources/\\\" target=\\\"_blank\\\">resources</a>\\n\" +\n" +
-                "                                tailored for student,"
-            ],
-            resource: {
-                type: "text",
-                details: [
-                    {
-                        title: "MIT Alumni Association",
-                        linkUrl: "https://alum.mit.edu/"
-                    },
-                    {
-                        title: "Alumni Directory",
-                        linkUrl: "https://alum.mit.edu/"
-                    },
-                    {
-                        title: "Alumni Career Resources",
-                        linkUrl: "https://alum.mit.edu/"
-                    },
-                    {
-                        title: "Lifelong Learning",
-                        linkUrl: "https://alum.mit.edu/"
-                    },
-                    {
-                        title: "Give to MIT",
-                        linkUrl: "https://alum.mit.edu/"
-                    },
-                    {
-                        title: "MIT Alumni Association",
-                        linkUrl: "https://alum.mit.edu/"
-                    }
-                ]
-            }
-        },
+        }
+		
+		/*,
         {
             name: "孙小风",
             descs: [
@@ -153,6 +100,6 @@ var teamData = {
                     }
                 ]
             }
-        }
-    ]*/
+        }*/
+    ]
 };
