9d075e2cefc9e7b62fde396e4a478c597bff86a4
ehcoo <garay.le8@gmail.com>
2021-03-08 20:19:38 +0800

    update the partner

--- zh/javascript/data/parnter.js
+++ zh/javascript/data/parnter.js
@@ -3,31 +3,23 @@ var parnterData = {
         {
             title: "Intensely curious" ,
             details: [
-                "In our undergraduate, graduate, and professional admissions, we seek applicants whose\n" +
-                " trengths, interests, and values are a good match for MIT. Because the Institute is built on\n" +
-                " the idea that talent and good ideas can come from anywhere, we are a <a\n" +
-                " href=\"http://diversity.mit.edu\" target=\"_blank\">remarkably diverse community</a>,\n" +
-                " drawing students from all 50 states and from 118 countries. Many are members of the first\n" +
-                " for many <a href=\"https://capd.mit.edu\" target=\"_blank\">careers</a> — and practical\n" +
-                " preparation for life."
+                "XXXX"
             ],
             resource: {
                 type: "vedio",
-                linkUrl: "http://web.mit.edu/files/images/201807/admissions-aid-3.mp4",
+                linkUrl: "../files/images/hpb.mp4",
                 desc: "Your browser does not support the video tag"
             }
         },
         {
-            title: "Intensely curious" ,
+            title: "XXXX" ,
             details: [            
-                "                        for many <a href=\"https://capd.mit.edu\" target=\"_blank\">careers</a> — and practical\n" +
-                "                        preparation for life.",
-                "In our undergraduate, graduate, and professional admissions, we seek applicants whose\n" 
+                " for many <a href=\"1\" target=\"_blank\">XXXX</a>" 
             ],
             resource: {
                 type: "image",
                 linkUrl: "../files/images/202003/MIT-Astronaut-Isolation-SL.jpg",
-                desc: "MIT Alumni Association"
+                desc: "XXXX"
             }
         }
     ]
