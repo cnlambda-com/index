a64dce5c2600774572a39ffc5785d76752e24041
ehcoo <garay.le8@gmail.com>
2021-03-08 19:59:58 +0800

    update the research page

--- zh/javascript/data/research.js
+++ zh/javascript/data/research.js
@@ -1,41 +1,37 @@
 var researchData = {
     sections:[
         {
-            title: "At MIT, pushing the boundaries of knowledge and possibility is our joyful obsession",
-            contents: [
-                "Research flourishes in our 30 departments across <a\n" +
-                "                                href=\"../education/index.html#schools-and-departments\">five schools</a> and <a\n" +
-                "                                href=\"http://computing.mit.edu/\">one college</a>, as well as in dozens of <a\n" +
-                "                                href=\"centers-labs-programs/index.html\">centers, labs, and programs</a> that convene\n" +
-                "                            experts across disciplines to explore new intellectual frontiers and attack important\n" +
-                "                            societal problems. Our on-campus research capabilities are enhanced through the work of MIT\n" +
-                "                            <a href=\"https://www.ll.mit.edu/\" target=\"_blank\">Lincoln Laboratory</a>, the <a\n" +
-                "                                    href=\"http://www.whoi.edu/\" target=\"_blank\">Woods Hole Oceanographic Institution</a>,\n" +
-                "                            active research <a href=\"http://ilp.mit.edu/\" target=\"_blank\">relationships with\n" +
-                "                                industry</a>, and a wide range of <a href=\"http://global.mit.edu/\" target=\"_blank\">global\n" +
-                "                                collaborations</a>. In this work, the <a href=\"https://libraries.mit.edu/\"\n" +
-                "                                                                         target=\"_blank\">MIT Libraries</a> serve as a\n" +
-                "                            crucial partner and a source of important research in their own right."
+            title: "可信执行环境 + 区块链",
+            contents:[
+            "MIT continually develops organizations and partnerships that foster interdisciplinary"
             ],
             mediaFile: {
                 type: "vedio",
-                linkUrl: "http://web.mit.edu/files/images/201805/research-2_0.mp4",
+                linkUrl: "../files/images/hpb.mp4",
                 desc: "Your browser does not support the video tag."
             }
         },
         {
-            title: "Centers, Labs &amp; Programs",
+            title: "人工智能 + 区块链",
             contents:[
-            "MIT continually develops organizations and partnerships that foster interdisciplinary\n" +
-            "                                work. Listed here are just some of the MIT labs, centers, and programs where\n" +
-            "                                groundbreaking research is happening every day.",
-            "<a class=\"tle-section__cta\" href=\"centers-labs-programs/index.html\">View Centers, Labs\n" +
-            "                                &amp; Programs</a>"
+            "MIT continually develops organizations and partnerships that foster interdisciplinary"
+            
             ],
             mediaFile: {
             type: "image",
                 linkUrl: "../files/images/202003/MIT-Astronaut-Isolation-SL.jpg",
                 desc: "MIT researchers xxx "
+            }
+        },
+		{
+            title: "At MIT",
+            contents: [
+                "Research flourishes in our 30 departments across"
+            ],
+            mediaFile: {
+                type: "vedio",
+                linkUrl: "../files/images/hpb.mp4",
+                desc: "Your browser does not support the video tag."
             }
         }
     ]
