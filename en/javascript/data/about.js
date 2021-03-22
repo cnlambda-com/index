79f5a49a78dfc53ab90b07d4223c28e0b75acd5a
cnlambda-com <cnlambda_com@163.com>
2021-03-22 15:13:26 +0800

    en-about update

--- en/javascript/data/about.js
+++ en/javascript/data/about.js
@@ -3,10 +3,38 @@ var aboutData = {
         {
             contents:[
                 "About us" ,     				
-                "Founded to accelerate.\n" +
-                "mission of <a href=\"#mission\">service</a>."
+                "xxxxx"
             ],
-			contents1:[
+        }/*,
+        {
+            contents:[
+                "Governance model" ,
+                "<div class="tle-section__content">"+
+              "<div class="tle-section__description">"+ 
+               "<p>xxxxx</p>"+ 
+               "<h4>Vision</h4> "+
+               "<p>xxxxx</p>"+ 
+               "<h4>Outcomes</h4> "+
+               "<p>xxxxx</p>"+ 
+               "<li class="installation-downloads-item"><span>Windows</span> <a href="http://get.adobe.com/air/"><span>Download</span></a></li>"+
+              "</div> "+
+             "</div>"+ 
+             "<figure class="image-grid tle-section__image-grid image-grid--single image-grid--1"> "+
+              "<div class="image-grid__inner">"+ 
+               "<div class="image-grid__grid-item"> "+
+                "<div class="image-grid__image"> "+
+                 "<img class="lazyloaded" data-src="../files/images/202003/4.png" src="../files/images/202003/4.png" alt="团队Logo" />"+
+                "</div> "+
+                "<figcaption class="image-grid__captions" aria-hidden="true"> "+
+                 "<a href="../files/images/202003/4.png" download="CNlambda-logo"><span class="image-grid__caption">Download</span></a>"+
+                "</figcaption> "+
+               "</div> "+
+              "</div>"+ 
+             "</figure>"
+            ]
+        },
+        {
+            contents:[
                 "Research flourishes" ,     			
                 
                 "<div class='gallery-images js-gallery-images'>" +
@@ -19,14 +47,8 @@ var aboutData = {
                 "</figure>"+
             "</div>"
             ]
-        },
-        {
-            contents:[
-                "About us" ,
-                "Contents..."
-            ]
         }
-
+*/
     ]
 };
 
--- zh/javascript/data/about.js
+++ zh/javascript/data/about.js
@@ -4,10 +4,7 @@ var aboutData = {
             contents:[
                 "关于我们" 
 				,
-                " With ingenuity and drive, our graduates have invented fundamental technologies, launched" +
-				  " With ingenuity and drive, our graduates have invented fundamental technologies, launched" +
-				    " With ingenuity and drive, our graduates have invented fundamental technologies, launched" +
-                " new industries, and created millions of American jobs. At the same time, and without the"
+                " xxxxx"
             ]
         }
     ]
