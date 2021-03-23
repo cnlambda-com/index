7eeb73b0f670becf741480e8cc87dd4eadaf4528
cnlambda-com <cnlambda_com@163.com>
2021-03-23 17:13:41 +0800

    research updated

--- en/javascript/data/research.js
+++ en/javascript/data/research.js
@@ -36,15 +36,9 @@ var researchData = {
 		{
             title: "Artificial Intelligence + Blockchain",
             contents:[
-            "Random numbers are an essential building block for a variety of applications " + 
-			"such as key generation and sample selection. Our project aims to utilize a  " + 
-			"group of blockchain miners (enabled <a href=\"https://en.wikipedia.org/wiki/Field-programmable_gate_array\" target=\"_blank\"> FPGA </a>chips) as the seeds to " + 
-			"generate the random number. Each blockchain miner submits a commitment associated " + 
-			"with the seed and reveals the cryptographic commitment to obtain a random number " + 
-			"in an epoch. The security generated numbers rely on the assumption that " + 
-			"there is at least one honest miner." +
-            "<a class=\"tle-section__cta\" href=\"centers-labs-programs/index.html\">View Centers, Labs\n" +
-            "                                &amp; Programs</a>"
+            "xxxxx",
+            "<a class=\"tle-section__cta\" href=\"centers-labs-programs/index.html\">xxx\n" +
+            "                                &amp; xxx</a>"
             ],
             items:[
                 {
@@ -80,8 +74,8 @@ var researchData = {
             "xxxxx\n" +
             "xxx\n" +
             "xx",
-            "<a class=\"tle-section__cta\" href=\"centers-labs-programs/index.html\">View Centers, Labs\n" +
-            "                                &amp; Programs</a>"
+            "<a class=\"tle-section__cta\" href=\"centers-labs-programs/index.html\">xxx\n" +
+            "                                &amp; xx</a>"
             ],
             items:[
                 {
