bca94339bbaa353bb99e975930b66b319323b559
cnlambda-com <cnlambda_com@163.com>
2021-04-22 15:07:51 +0800

    update

--- en/javascript/data/research.js
+++ en/javascript/data/research.js
@@ -4,7 +4,8 @@ var researchData = {
             title: "Finger Feature-based Random Verification Code Enhancement Technology",
             url:"../news/chenwei.html",
             contents:[
-            "xxxxx"
+            "Pure number based random verification codes are widely used in mobile payments, for example, large payment platforms such as Alipay and PayPal all use random verification codes to verify user identity. However, as the main receiver of pure digital verification codes, SIM cards are easy to be copied. In addition, due to the existence of pseudo signal stations, pure digital verification codes are easy to be hijacked or stolen. Once the thief obtains the random verification code, he can easily crack the identity verification barrier of the payment platform and conduct illegal operations, which brings great risks to mobile payment. While, due to the characteristics of the pure digital verification code, it is difficult for us to enhance its reliability and security."
+            +"<p><a class=\"tle-section__cta\" href=\"../news/chenwei.html\">Learn more\n</a></p>" 
             ],
             mediaFile: {
                 type: "image",
@@ -16,7 +17,8 @@ var researchData = {
             title: "Fraud Detection on Blockchain: A Deep Learning-based Representation Learning approach",
             url:"../news/talooria.html",
             contents:[
-            "xxxxx"
+            "Recently, blockchain becomes a popular cryptocurrency scheme, with a digital decentralized ledger in which all transactions are recorded chronologically and publicly. One of blockchain’s primary advantages is making reliable transactions among a group of untrusted participants, so it makes anomaly detection extremely important to preserve its essence and effectiveness. "
+            +"<p><a class=\"tle-section__cta\" href=\"../news/chenwei.html\">Learn more\n</a></p>" 
             ],
             mediaFile: {
                 type: "image",
@@ -28,8 +30,8 @@ var researchData = {
             title: "Blockchain Layer 2 Scaling Solutions",
             url:"../news/shuangw.html",
             contents:[
-            "xxxxx"
-            //"<a class=\"tle-section__cta\" href=\"centers-labs-programs/index.html\">xxx\n" +
+            "Blockchain layer 2 scaling solutions are aiming to increase the throughput of blockchain and decrease the transaction fee without changing the underlying blockchain structure. The current layer 2 solution such as zk-Rollup, Plasma, Nocust … they share a same idea that a operator over blockchain is introduced to the system, the operator handle users’ transactions and does off-line computations.  These systems are focus on moving vast of the data and computation offline so the blockchain will handle less data, while most of them do not introduce privacy aspect to their system yet. In these system, users’ balance and transaction values are all transparent, which is not desirable for privacy. Privacy and scalability, however, are hard to achieve at the same time. Since adding privacy and confidentiality for a blockchain inevitably adds more computation and data to the blockchain, reducing the transaction throughput and increasing the transaction fees, thus downgrading the scalability of the system. There is always a trade-off between these two aspects."
+            +"<p><a class=\"tle-section__cta\" href=\"../news/chenwei.html\">Learn more\n</a></p>" //"<a class=\"tle-section__cta\" href=\"centers-labs-programs/index.html\">xxx\n" +
             //"                                &amp; xxx</a>"
             ],
             mediaFile: {
@@ -42,8 +44,8 @@ var researchData = {
             title: "Artificial Intelligence(Deep Learning) Based Transaction Data Analysis of the Blockchain",
             url:"../news/shanzhu.html",
             contents:[
-                "xxxxx\n"
-                ],
+                "Benefited from the anonymity and great value of the blockchain, digital currency and the decentralized finance (DeFi) built on it has also become a trend. However, the encryption and anonymity of virtual currencies also provide many illegal sellers with technical advantages, such as encrypted crimes, ransomware, money laundering, and so on. Therefore, based on the openness and transparency of transaction records on the chain, we aim to use artificial intelligence, especially the deep learning methods, to mine more information, and to help government agencies as well as the enterprises analyze, understand and supervise the transactions."
+                +"<p><a class=\"tle-section__cta\" href=\"../news/chenwei.html\">Learn more\n</a></p>" ],
             mediaFile: {
                 type: "image",
                 linkUrl: "../files/images/research/shanzhu.jpg",
