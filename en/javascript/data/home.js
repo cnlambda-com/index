3d706abf890472e5a74d412156b8c62ecd460e6c
cnlambda-com <cnlambda_com@163.com>
2021-04-24 15:42:55 +0800

    home update

--- en/javascript/data/home.js
+++ en/javascript/data/home.js
@@ -1,7 +1,9 @@
 var homeData = {
     sections: [
         {
-            title: " <a href=\"https://cnlambda.com\" target=\"_blank\">CNlambda (CN<sup>&reg;</sup>&lambda;)</a> is a nonprofit enterprise-focused cryptography and artificial intelligence research group. <a href=\"https://cnlambda.com\" target=\"_blank\">CN<sup>&reg;</sup>&lambda;</a> aims to solve the practical security and privacy problems for the enterprise, from threat detection, security modeling, protocol design, to implementations of real applications. <a href=\"https://cnlambda.com\" target=\"_blank\">CN<sup>&reg;</sup>&lambda;</a> brings together researchers, business leaders, entrepreneurs in a catalytic environment to produce visionary results that will have an impact on people's real life.",
+            title: " <a href=\"https://cnlambda.com\" target=\"_blank\">CNlambda (CN<sup>&reg;</sup>&lambda;)</a> is a nonprofit enterprise-focused cryptography research group."
+            +" <a href=\"https://cnlambda.com\" target=\"_blank\">CN<sup>&reg;</sup>&lambda;</a> aims to solve the practical security and privacy problems for the enterprise via under the help of Artificial Intelligence. The service includes, intelligent threat detection, intelligent security modeling, protocol design, and the implementations of real applications. "
+            +"<a href=\"https://cnlambda.com\" target=\"_blank\">CN<sup>&reg;</sup>&lambda;</a> is focusing on how to effectively use artificial intelligence to strengthen the security and privacy of related industries, then produces results that can benefit enterprises and improve the quality of people life.",
             hotContents: [
                 {
                     title: "XXXXXXXXXXXXX",
