a8c75a357e1f5e31a2fccf5c07b150b998dece1d
cnlambda-com <cnlambda_com@163.com>
2021-06-01 16:12:50 +0800

    new tab achievement

---
+++ en/javascript/data/product.js
@@ -0,0 +1,65 @@
+var productData = {
+    sections:[
+		{
+            title: "Products",
+            items:[
+                {
+                    name:"Visit MIT",
+                    url:"/visitmit"
+                },
+                {
+                    name:"Kendall Square",
+                    url:"https://www.kendallsq.org"
+                },
+                {
+                    name:"Visiting Boston",
+                    url:"https://www.boston.gov/visiting-boston"
+                }
+            ],
+        }
+    ],
+    figure2x2:[
+        {
+            src:"../files/images/logo.png",
+            alt:"Studying on Killian Court",
+            discription:"discription"
+        }
+    ],
+    figure1x1:[
+        {
+            src:"../files/images/logo.png",
+            alt:"Studying on Killian Court",
+            discription:"discription"
+        },
+        {
+            src:"../files/images/logo.png",
+            alt:"Studying on Killian Court",
+            discription:"discription"
+        },
+        {
+            src:"../files/images/logo.png",
+            alt:"Studying on Killian Court",
+            discription:"discription"
+        },
+        {
+            src:"../files/images/logo.png",
+            alt:"Studying on Killian Court",
+            discription:"discription"
+        },
+        {
+            src:"../files/images/logo.png",
+            alt:"Studying on Killian Court",
+            discription:"discription"
+        },
+        {
+            src:"../files/images/logo.png",
+            alt:"Studying on Killian Court",
+            discription:"discription"
+        },
+        {
+            src:"../files/images/logo.png",
+            alt:"Studying on Killian Court",
+            discription:"discription"
+        }
+    ]
+};
