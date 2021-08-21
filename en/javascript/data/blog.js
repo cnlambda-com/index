aa7525639b31aa0adcc889ff4476e3a655eea199
cnlambda-com <cnlambda_com@163.com>
2021-08-21 12:19:26 +0800

    blog added

---
+++ en/javascript/data/blog.js
@@ -0,0 +1,19 @@
+var blogData={
+    sections:[
+		{
+            title: "Martislab Research Newsletters",
+            url:"",
+            contents:[
+                {
+                    ttl:"Martislab Research Newsletter, August 21, 2021",
+                    url:"../files/documents/martislab宣传册_en.pdf"
+                },
+            ],
+            mediaFile: {
+                type: "",
+                linkUrl: "",
+                desc: ""
+            }
+        },
+    ]
+}
---
+++ zh/javascript/data/blog.js
@@ -0,0 +1,19 @@
+var blogData={
+    sections:[
+		{
+            title: "Martislab 研究简报",
+            url:"",
+            contents:[
+                {
+                    ttl:"Martislab Research Newsletter, August 21, 2021",
+                    url:"../files/documents/martislab宣传册.pdf"
+                },
+            ],
+            mediaFile: {
+                type: "",
+                linkUrl: "",
+                desc: ""
+            }
+        },
+    ]
+}
